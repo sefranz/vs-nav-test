dfd592c3e2849ca84757e1e0a6a0bbceb30dc102
Sebastian Franz <sebastian.franz@online.de>
2021-11-13 11:45:01 +0100

    Initial setup

---
+++ src/components/HelloWorlder.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelloWorlder = () => (
+  <div>
+    Hello World
+  </div>
+);
+
+export default HelloWorlder;
---
+++ src/pages/test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorlder from '../components/HelloWorlder';
+
+const Login = () => {
+  console.log('Hello')
+
+  return (
+    <div>
+      <HelloWorlder />
+    </div>
+  );
+};
+
+export default Login;
